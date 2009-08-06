69a0bfdef887d2c2701eaa58c9c7fe8f18afc469
Gregory Crosswhite <gcross@phys.washington.edu>
2009-08-06 15:55:49 -0700

    Removed "vpi_" from functions in "xij"

--- src/xij.f95
+++ src/xij.f95
@@ -15,8 +15,8 @@ elemental function wrap_around(x,period_length)
   wrap_around = x - period_length*(floor(x/period_length - 0.5D0) + 1.0D0)
 end function
 !@-node:gcross.20090805153643.1842:wrap_around
-!@+node:gcross.20090805093617.1833:vpi_update_xij
-subroutine vpi_update_xij( xij2, x, sl_start, sl_end, N_SLICES, N_PARTICLES, N_DIMENSIONS )
+!@+node:gcross.20090805093617.1833:update_xij
+subroutine update_xij( xij2, x, sl_start, sl_end, N_SLICES, N_PARTICLES, N_DIMENSIONS )
   integer, intent(in) :: N_SLICES, N_PARTICLES, N_DIMENSIONS
   integer, intent(in) :: sl_start, sl_end
   double precision, dimension ( N_SLICES, N_PARTICLES , N_PARTICLES ), intent(inout) :: xij2
@@ -27,10 +27,10 @@ subroutine vpi_update_xij( xij2, x, sl_start, sl_end, N_SLICES, N_PARTICLES, N_D
   forall (s=sl_start:sl_end, i=1:N_PARTICLES, j=1:N_PARTICLES) &
     xij2(s,i,j) = sum( (x(s,i,:) - x(s,j,:))**2 )
 
-end subroutine vpi_update_xij
-!@-node:gcross.20090805093617.1833:vpi_update_xij
-!@+node:gcross.20090805093617.1834:vpi_update_xij_pbc
-subroutine vpi_update_xij_pbc( xij2, x, period_length, sl_start, sl_end, N_SLICES, N_PARTICLES, N_DIMENSIONS )
+end subroutine update_xij
+!@-node:gcross.20090805093617.1833:update_xij
+!@+node:gcross.20090805093617.1834:update_xij_pbc
+subroutine update_xij_pbc( xij2, x, period_length, sl_start, sl_end, N_SLICES, N_PARTICLES, N_DIMENSIONS )
   integer, intent(in) :: N_SLICES, N_PARTICLES, N_DIMENSIONS
   integer, intent(in) :: sl_start, sl_end
   double precision, dimension ( N_SLICES, N_PARTICLES , N_PARTICLES ), intent(inout) :: xij2
@@ -42,8 +42,8 @@ subroutine vpi_update_xij_pbc( xij2, x, period_length, sl_start, sl_end, N_SLICE
   forall (s=sl_start:sl_end, i=1:N_PARTICLES, j=1:N_PARTICLES) &
     xij2(s,i,j) = sum( wrap_around( (x(s,i,:) - x(s,j,:)), period_length )**2 )
 
-end subroutine vpi_update_xij_pbc
-!@-node:gcross.20090805093617.1834:vpi_update_xij_pbc
+end subroutine update_xij_pbc
+!@-node:gcross.20090805093617.1834:update_xij_pbc
 !@-others
 
 end module xij
