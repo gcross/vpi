7aa96a8dd4d28d3815c015bb2574696e38b3fecd
Gregory Crosswhite <gcross@phys.washington.edu>
2009-08-12 11:54:21 -0700

    Completed wrapping of compute_physical_potential

--- src/tests/thermalize.py
+++ src/tests/thermalize.py
@@ -5,9 +5,10 @@
 
 import unittest
 from paycheck import with_checker
-from paycheck.generator import positive_float, non_negative_float
-from numpy import zeros, double
+from paycheck.generator import positive_float, non_negative_float, irange
+from numpy import array, zeros, double, float64
 from numpy.linalg import norm
+from numpy.random import rand
 from tests import particle_paths_type
 import vpi
 
@@ -21,10 +22,95 @@ class accept_path(unittest.TestCase):
             (p1 < p2)
         )
 #@-node:gcross.20090807144330.2152:accept_path
+#@+node:gcross.20090812093015.1718:compute_potential
+class compute_physical_potential(unittest.TestCase):
+    #@    @+others
+    #@+node:gcross.20090812093015.1719:test_null_case
+    @with_checker(
+            irange(1,5),
+            irange(1,5),
+            irange(1,5),
+            irange(1,5),
+        number_of_calls=10
+    )
+    def test_null_case(self,
+            n_slices,
+            n_particles,
+            n_dimensions,
+            n_dimensions_rotation
+        ):
+        x = array(rand(n_slices,n_particles,n_dimensions),order='Fortran')
+        xij2 = array(rand(n_slices,n_particles,n_particles),order='Fortran')
+        x_rot = array(zeros((n_slices,n_particles,n_dimensions_rotation)),order='Fortran')
+        def null_func(*args):
+            return double(0.0)
+        def Uij_func(*args):
+            return 0.0, False
+        gnull = array(zeros((n_particles,n_dimensions)),dtype=double,order='Fortran')
+        def null_grad_func(*args):
+            return gnull
+        U,gradU2,reject_flag = vpi.thermalize.compute_physical_potential(
+            x,xij2,
+            null_func,
+            null_grad_func,
+            Uij_func,
+            null_grad_func,
+            1,n_slices,
+            x_rot=x_rot,
+            Uij_rot_func=null_func
+            )
+        self.failIf(reject_flag)
+        self.assert_((U==0).all())
+        self.assert_((gradU2==0).all())
+    #@nonl
+    #@-node:gcross.20090812093015.1719:test_null_case
+    #@+node:gcross.20090812093015.1721:test_constant_case
+    @with_checker(
+            irange(1,5),
+            irange(1,5),
+            irange(1,5),
+            irange(1,5),
+        number_of_calls=10
+    )
+    def test_constant_case(self,
+            n_slices,
+            n_particles,
+            n_dimensions,
+            n_dimensions_rotation
+        ):
+        x = array(rand(n_slices,n_particles,n_dimensions),order='Fortran')
+        xij2 = array(rand(n_slices,n_particles,n_particles),order='Fortran')
+        x_rot = array(zeros((n_slices,n_particles,n_dimensions_rotation)),order='Fortran')
+        def null_func(*args):
+            return double(0.0)
+        def constant_func(*args):
+            return double(1.0)
+        def Uij_func(*args):
+            return double(1.0), False
+        gnull = array(zeros((n_particles,n_dimensions)),dtype=double,order='Fortran')
+        def null_grad_func(*args):
+            return gnull
+        U,gradU2,reject_flag = vpi.thermalize.compute_physical_potential(
+            x,xij2,
+            constant_func,
+            null_grad_func,
+            Uij_func,
+            null_grad_func,
+            1,n_slices,
+            x_rot=x_rot,
+            Uij_rot_func=null_func
+            )
+        self.failIf(reject_flag)
+        self.assert_((U==2).all())
+        self.assert_((gradU2==0).all())
+    #@-node:gcross.20090812093015.1721:test_constant_case
+    #@-others
+#@-node:gcross.20090812093015.1718:compute_potential
 #@-others
 
 tests = [
-    accept_path
+    accept_path,
+    compute_physical_potential,
     ]
 
 if __name__ == "__main__":
--- src/thermalize.f90
+++ src/thermalize.f90
@@ -24,6 +24,122 @@ function accept_path( lngfn0, lngfn1 ) result( accept )
 
 end function accept_path
 !@-node:gcross.20090623152316.32:accept_path
+!@+node:gcross.20090721121051.1764:compute_physical_potential
+subroutine compute_physical_potential (&
+    x, xij2, x_rot, &
+    Usp_func, gUsp_func, &
+    Uij_func, gUij_func, &
+    move_start, move_end, &
+    n_slices, n_particles, n_dimensions, n_dimensions_rotation, &
+    U, gradU2, &
+    reject_flag, &
+    Uij_rot_func &
+    )
+  implicit none
+!@+at
+! Array dimensions / slicing
+!@-at
+!@@c
+  integer, intent(in) :: move_start, move_end, n_slices, n_particles, n_dimensions, n_dimensions_rotation
+!@+at
+! Function input
+!@-at
+!@@c
+  double precision, dimension ( n_slices, n_particles, n_dimensions ), intent(in) :: x
+  double precision, dimension ( n_slices, n_particles, n_particles ), intent(in) :: xij2
+  double precision, dimension ( n_slices, n_particles, n_dimensions_rotation ), intent(in), optional :: x_rot
+!@+at
+! Potential functions
+!@-at
+!@@c
+!f2py external, intent(callback) :: Usp_func, gUsp_func, Uij_func, gUij_func
+!f2py external, intent(callback), optional :: Uij_rot_func
+  optional :: Uij_rot_func
+interface
+  !@  << Callback interface >>
+  !@+node:gcross.20090809223137.1724:<< Callback interface >>
+  function Usp_func( x, slice, ip, nslice, np, ndim ) result ( Usp )
+    integer, intent(in) :: nslice, np, ndim
+    integer, intent(in) :: slice, ip
+    double precision, dimension ( nslice, np , ndim ), intent(in) :: x
+    double precision :: Usp
+  end function Usp_func
+
+  subroutine gUsp_func( x, slice, nslice, np, ndim, gUsp )
+    double precision, dimension ( nslice, np , ndim ), intent(in) ::  x
+    integer, intent(in) :: slice, nslice, np, ndim
+    double precision, dimension ( np, ndim ), intent(out) :: gUsp
+  end subroutine gUsp_func
+
+  function Uij_func( x, xij2, slice, ip, nslice, np, ndim, reject_flag ) result ( Uij )
+    double precision, dimension ( nslice, np , ndim ), intent(in) :: x
+    double precision, dimension ( nslice, np , np ), intent(in) :: xij2
+    integer, intent(in) :: slice, ip, nslice, np, ndim
+    logical, intent(out) :: reject_flag
+    double precision :: Uij
+  end function Uij_func
+
+  subroutine gUij_func( x, xij2, slice, nslice, np, ndim, gUij )
+    double precision, dimension ( nslice, np , ndim ), intent(in) :: x
+    double precision, dimension ( nslice, np , np ), intent(in) :: xij2
+    integer, intent(in) :: slice, nslice, np, ndim
+    double precision, dimension ( np , ndim ), intent(out) :: gUij
+  end subroutine gUij_func
+
+  function Uij_rot_func( x, x_rot, xij2, slice, ip, nslice, np, ndim, ndim_rot ) result ( Uij_rot )
+    double precision, dimension ( nslice, np , ndim ), intent(in) :: x
+    double precision, dimension ( nslice, np , ndim_rot ), intent(in) :: x_rot
+    double precision, dimension ( nslice, np , np ), intent(in) :: xij2
+    integer, intent(in) :: slice, ip, nslice, np, ndim, ndim_rot
+    double precision :: Uij_rot
+  end function Uij_rot_func
+  !@-node:gcross.20090809223137.1724:<< Callback interface >>
+  !@nl
+end interface
+!@+at
+! Function output
+!@-at
+!@@c
+  double precision, dimension( n_slices, n_particles ), intent(out) :: U
+  double precision, dimension( n_slices ), intent(out) :: gradU2
+  logical, intent(out) :: reject_flag
+!@+at
+! Local variables.
+!@-at
+!@@c
+  double precision, dimension( n_particles, n_dimensions ) :: grad_Usp, grad_Uij, grad_Uij_rot, grad_U
+  double precision :: U_sp, U_ij, U_ij_rot
+  integer :: ii, jj
+!@+at
+! Code begins:
+!@-at
+!@@c
+
+  reject_flag = .false.
+
+  do ii = move_start, move_end
+    U(ii,:) = 0.0d0
+    do jj = 1, n_particles
+      U_sp = Usp_func( x, ii, jj, n_slices, n_particles, n_dimensions )
+      U_ij = Uij_func( x, xij2, ii, jj, n_slices, n_particles, n_dimensions, reject_flag )
+      if(reject_flag) then
+          return
+      end if
+      U_ij_rot=0
+      if( PRESENT(Uij_rot_func) ) then
+        U_ij_rot = Uij_rot_func( x, x_rot, xij2, ii, jj, n_slices, n_particles, n_dimensions, n_dimensions_rotation )
+      end if
+      U(ii,jj) = U(ii,jj) + ( U_sp + U_ij + U_ij_rot )
+    end do
+
+    call gUsp_func( x, ii, n_slices, n_particles, n_dimensions, grad_Usp )
+    call gUij_func( x, xij2, ii, n_slices, n_particles, n_dimensions, grad_Uij )
+    grad_U = grad_Usp + grad_Uij
+    gradU2(ii) = sum( grad_U(:,:)**2 )
+  end do
+
+end subroutine
+!@-node:gcross.20090721121051.1764:compute_physical_potential
 !@-others
 
 end module thermalize
