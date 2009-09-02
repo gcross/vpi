eb25588d364c9a93b21267c9d35a30195295b1ce
Gregory Crosswhite <gcross@phys.washington.edu>
2009-09-02 14:49:01 -0700

    Changed name of argument in harmonic_oscillator
    Goal is to make explicit the fact that the potential
    functions and the trial functions require different
    (though related) sets of coefficients.

--- src/harmonic_oscillator.f95
+++ src/harmonic_oscillator.f95
@@ -11,12 +11,12 @@ contains
 !@+node:gcross.20090901084550.2619:compute_trial_weight
 pure function compute_trial_weight( &
     x, &
-    harmonic_oscillator_coefficients, &
+    trial_coefficients, &
     n_particles, n_dimensions &
   ) result( weight )
   integer, intent(in) :: n_particles, n_dimensions
   double precision, dimension( n_particles, n_dimensions ), intent(in) :: x
-  double precision, dimension( n_dimensions ), intent(in) :: harmonic_oscillator_coefficients
+  double precision, dimension( n_dimensions ), intent(in) :: trial_coefficients
   double precision  :: weight
 
   double precision, dimension( n_dimensions ) :: temp
@@ -25,46 +25,46 @@ pure function compute_trial_weight( &
   forall (i=1:n_dimensions) &
     temp(i) = sum(x(:,i)**2)
 
-  weight = -dot_product(temp,harmonic_oscillator_coefficients)/2d0
+  weight = -dot_product(temp,trial_coefficients)/2d0
 end function
 !@-node:gcross.20090901084550.2619:compute_trial_weight
 !@+node:gcross.20090901084550.2621:accumulate_trial_derivatives
 pure subroutine accumulate_trial_derivatives( &
     x, &
-    harmonic_oscillator_coefficients, &
+    trial_coefficients, &
     n_particles, n_dimensions, &
     gradient_of_log_trial_fn, laplacian_of_log_trial_fn &
   )
   integer, intent(in) :: n_particles, n_dimensions
   double precision, dimension( n_particles, n_dimensions ), intent(in) :: x
-  double precision, dimension( n_dimensions ), intent(in) :: harmonic_oscillator_coefficients
+  double precision, dimension( n_dimensions ), intent(in) :: trial_coefficients
   double precision, dimension( n_particles, n_dimensions ), intent(inout) :: gradient_of_log_trial_fn
   double precision, intent(inout) :: laplacian_of_log_trial_fn
 
   integer :: i
 
   forall (i=1:n_dimensions) &
-    gradient_of_log_trial_fn(:,i) = gradient_of_log_trial_fn(:,i) - harmonic_oscillator_coefficients(i)*x(:,i)
+    gradient_of_log_trial_fn(:,i) = gradient_of_log_trial_fn(:,i) - trial_coefficients(i)*x(:,i)
 
-  laplacian_of_log_trial_fn = laplacian_of_log_trial_fn - sum(harmonic_oscillator_coefficients)*n_particles
+  laplacian_of_log_trial_fn = laplacian_of_log_trial_fn - sum(trial_coefficients)*n_particles
 end subroutine
 !@-node:gcross.20090901084550.2621:accumulate_trial_derivatives
 !@+node:gcross.20090901084550.2626:accumulate_potential
-pure subroutine accumulate_potential( &
+subroutine accumulate_potential( &
     x, &
-    harmonic_oscillator_coefficients, &
+    potential_coefficients, &
     n_slices, n_particles, n_dimensions, &
     U &
   )
   integer, intent(in) :: n_slices, n_particles, n_dimensions
   double precision, dimension( n_slices, n_particles, n_dimensions ), intent(in) :: x
-  double precision, dimension( n_dimensions ), intent(in) :: harmonic_oscillator_coefficients
+  double precision, dimension( n_dimensions ), intent(in) :: potential_coefficients
   double precision, dimension( n_slices, n_particles ), intent(inout)  :: U
 
   integer :: i,j
 
   forall (i=1:n_slices,j=1:n_particles) &
-    U(i,j) = U(i,j) + dot_product(harmonic_oscillator_coefficients,x(i,j,:)**2)/2d0
+    U(i,j) = U(i,j) + dot_product(potential_coefficients,x(i,j,:)**2)/2d0
 
 end subroutine
 !@-node:gcross.20090901084550.2626:accumulate_potential
