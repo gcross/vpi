89b51766b3fa657222be1562cc8ac468b2531745
Gregory Crosswhite <gcross@phys.washington.edu>
2009-09-11 14:11:48 -0700

    Update fancy phase angular momentum to new convention
    (Now it accumulates the full gradient rather then
    just the anglular derivatives.)

--- src/angular_momentum.f95
+++ src/angular_momentum.f95
@@ -73,55 +73,65 @@ pure function sum_over_symmetrizations(amplitudes,N_particles,N_rotating_particl
 
 end function
 !@-node:gcross.20090803153449.1836:sum_over_symmetrizations
-!@+node:gcross.20090827135657.1422:compute_angular_derivatives
-pure subroutine compute_angular_derivatives(&
+!@+node:gcross.20090827135657.1422:accumulate_gradient_fancy
+pure subroutine accumulate_gradient_fancy (&
     x, &
-    rotation_plane_axis_1, rotation_plane_axis_2, N_rotating_particles, &
-    N_particles,N_dimensions, &
-    derivatives &
+    n_rotating_particles, &
+    rotation_plane_axis_1, rotation_plane_axis_2, &
+    n_slices, n_particles, n_dimensions, &
+    gradient_phase &
   )
 
   ! Input variables
-  integer, intent(in) :: N_particles, N_dimensions
-  double precision, dimension(N_particles,N_dimensions), intent(in) :: x
-  integer, intent(in) :: rotation_plane_axis_1, rotation_plane_axis_2, N_rotating_particles
+  integer, intent(in) :: n_slices, n_particles, n_dimensions
+  double precision, dimension(n_slices, n_particles, n_dimensions), intent(in) :: x
+  integer, intent(in) :: rotation_plane_axis_1, rotation_plane_axis_2, n_rotating_particles
 
   ! Output variables
-  double precision, dimension(N_particles), intent(out) :: derivatives
+  double precision, dimension(n_slices, n_particles, n_dimensions), intent(inout) :: gradient_phase
 
   ! Local variables
-  double complex, dimension(N_particles) :: amplitudes
+  double complex, dimension(n_particles) :: amplitudes
   double complex :: saved_amplitude, full_sum, full_sum_conj, partial_sum
   double precision :: full_sum_amplitude_squared
-  integer :: i
+  integer :: s, i
 
-  if(N_rotating_particles == 0) then
-    derivatives = 0d0
+  if(n_rotating_particles == 0) then
     return
   end if
 
-  if(mod(N_particles,2) == 0 .and. N_rotating_particles == N_particles / 2) then
-    derivatives = 0.5d0
+  if(mod(n_particles,2) == 0 .and. n_rotating_particles == N_particles / 2) then
+    do i = 1, n_particles
+      do s = 1, n_slices
+        call accumulate_gradient(0.5d0,x(s,i,:),gradient_phase(s,i,:))
+      end do
+    end do
     return
   end if
 
-  if(N_rotating_particles == N_particles) then
-    derivatives = 1d0
+  if(n_rotating_particles == n_particles) then
+    do i = 1, n_particles
+      do s = 1, n_slices
+        call accumulate_gradient(1.0d0,x(s,i,:),gradient_phase(s,i,:))
+      end do
+    end do
     return
   end if
 
-  amplitudes(:) = &
-    (x(:,rotation_plane_axis_1)*(1.0d0,0) + x(:,rotation_plane_axis_2)*(0,1.0d0)) &
-  / sqrt(x(:,rotation_plane_axis_1)**2 + x(:,rotation_plane_axis_2)**2)
+  do s = 1, n_slices
 
-  if(N_rotating_particles == 1) then
-    full_sum = sum(amplitudes)
+    amplitudes(:) = &
+      (x(s,:,rotation_plane_axis_1)*(1.0d0,0) + x(s,:,rotation_plane_axis_2)*(0,1.0d0)) &
+    / sqrt(x(s,:,rotation_plane_axis_1)**2 + x(s,:,rotation_plane_axis_2)**2)
+    full_sum = sum_over_symmetrizations(amplitudes,n_particles,n_rotating_particles)
     full_sum_conj = conjg(full_sum)
-    full_sum_amplitude_squared = real(full_sum*conjg(full_sum))
-    derivatives(:) = real(amplitudes(:)*full_sum_conj)/full_sum_amplitude_squared
-    return
-  end if
+    full_sum_amplitude_squared = real(full_sum*full_sum_conj)
 
+    if(n_rotating_particles == 1) then
+      do i = 1, n_particles
+        call compute_and_accumulate_gradient(amplitudes(i),x(s,i,:),gradient_phase(s,i,:))
+      end do
+      cycle
 !@+at
 ! To get the partial sums, i.e. the the sums which are required to have a
 ! selected coordinate "i" appear, we compute the full sum but set the
@@ -130,19 +140,48 @@ pure subroutine compute_angular_derivatives(&
 ! by "i"'s amplitude.
 !@-at
 !@@c
-  full_sum = sum_over_symmetrizations(amplitudes,N_particles,N_rotating_particles)
-  full_sum_conj = conjg(full_sum)
-  full_sum_amplitude_squared = real(full_sum*full_sum_conj)
-  do i = 1, N_particles
-    saved_amplitude = amplitudes(i)
-    amplitudes(i) = (0d0,0d0)
-    partial_sum = sum_over_symmetrizations(amplitudes,N_particles,N_rotating_particles-1)*saved_amplitude
-    derivatives(i) = real(partial_sum*full_sum_conj)/full_sum_amplitude_squared
-    amplitudes(i) = saved_amplitude
+    else
+      do i = 1, n_particles
+        saved_amplitude = amplitudes(i)
+        amplitudes(i) = (0d0,0d0)
+        partial_sum = sum_over_symmetrizations(amplitudes,n_particles,n_rotating_particles-1)*saved_amplitude
+        call compute_and_accumulate_gradient(partial_sum,x(s,i,:),gradient_phase(s,i,:))
+        amplitudes(i) = saved_amplitude
+      end do
+    end if
   end do
 
+  contains
+
+  pure subroutine compute_and_accumulate_gradient(amplitude,x,gradient_phase)
+    double complex, intent(in) :: amplitude
+    double precision, dimension(n_dimensions), intent(in) :: x
+    double precision, dimension(n_dimensions), intent(inout) :: gradient_phase
+    double precision :: phase_derivative
+
+    phase_derivative = real(amplitude*full_sum_conj)/full_sum_amplitude_squared
+
+    call accumulate_gradient(phase_derivative,x,gradient_phase)
+  end subroutine
+
+  pure subroutine accumulate_gradient(phase_derivative,x,gradient_phase)
+    double precision, intent(in) :: phase_derivative
+    double precision, dimension(n_dimensions), intent(in) :: x
+    double precision, dimension(n_dimensions), intent(inout) :: gradient_phase
+    double precision :: rot_x, rot_y, rot_r_squared
+
+    rot_x = x(rotation_plane_axis_1)
+    rot_y = x(rotation_plane_axis_2)
+    rot_r_squared = rot_x**2 + rot_y**2
+
+    gradient_phase(rotation_plane_axis_1) = gradient_phase(rotation_plane_axis_1) &
+      + phase_derivative * rot_y/rot_r_squared
+    gradient_phase(rotation_plane_axis_2) = gradient_phase(rotation_plane_axis_2) &
+      - phase_derivative * rot_x/rot_r_squared
+  end subroutine
+
 end subroutine
-!@-node:gcross.20090827135657.1422:compute_angular_derivatives
+!@-node:gcross.20090827135657.1422:accumulate_gradient_fancy
 !@+node:gcross.20090903090230.2072:accumumulate_effective_potential
 subroutine accumulate_effective_potential (&
     gradient_phase, &
