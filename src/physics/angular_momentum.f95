cc59e07f70a7696d261049e69d96145913835374
Gregory Crosswhite <gcross@phys.washington.edu>
2009-09-15 14:42:08 -0700

    Extracted accum_angle_drv_into_gradient into separate fn

--- src/physics/angular_momentum.f95
+++ src/physics/angular_momentum.f95
@@ -103,7 +103,7 @@ pure subroutine accumulate_gradient_fancy (&
   if(mod(n_particles,2) == 0 .and. n_rotating_particles == N_particles / 2) then
     do i = 1, n_particles
       do s = 1, n_slices
-        call accumulate_gradient(0.5d0,x(s,i,:),gradient_phase(s,i,:))
+        call accumulate_gradient(0.5d0,gradient_phase(s,i,:))
       end do
     end do
     return
@@ -112,7 +112,7 @@ pure subroutine accumulate_gradient_fancy (&
   if(n_rotating_particles == n_particles) then
     do i = 1, n_particles
       do s = 1, n_slices
-        call accumulate_gradient(1.0d0,x(s,i,:),gradient_phase(s,i,:))
+        call accumulate_gradient(1.0d0,gradient_phase(s,i,:))
       end do
     end do
     return
@@ -129,7 +129,7 @@ pure subroutine accumulate_gradient_fancy (&
 
     if(n_rotating_particles == 1) then
       do i = 1, n_particles
-        call compute_and_accumulate_gradient(amplitudes(i),x(s,i,:),gradient_phase(s,i,:))
+        call compute_and_accumulate_gradient(amplitudes(i),gradient_phase(s,i,:))
       end do
       cycle
 !@+at
@@ -145,7 +145,7 @@ pure subroutine accumulate_gradient_fancy (&
         saved_amplitude = amplitudes(i)
         amplitudes(i) = (0d0,0d0)
         partial_sum = sum_over_symmetrizations(amplitudes,n_particles,n_rotating_particles-1)*saved_amplitude
-        call compute_and_accumulate_gradient(partial_sum,x(s,i,:),gradient_phase(s,i,:))
+        call compute_and_accumulate_gradient(partial_sum,gradient_phase(s,i,:))
         amplitudes(i) = saved_amplitude
       end do
     end if
@@ -153,35 +153,54 @@ pure subroutine accumulate_gradient_fancy (&
 
   contains
 
-  pure subroutine compute_and_accumulate_gradient(amplitude,x,gradient_phase)
+  pure subroutine compute_and_accumulate_gradient(amplitude,gradient_phase)
     double complex, intent(in) :: amplitude
-    double precision, dimension(n_dimensions), intent(in) :: x
     double precision, dimension(n_dimensions), intent(inout) :: gradient_phase
     double precision :: phase_derivative
 
     phase_derivative = real(amplitude*full_sum_conj)/full_sum_amplitude_squared
 
-    call accumulate_gradient(phase_derivative,x,gradient_phase)
+    call accumulate_gradient(phase_derivative,gradient_phase)
   end subroutine
 
-  pure subroutine accumulate_gradient(phase_derivative,x,gradient_phase)
+  pure subroutine accumulate_gradient(phase_derivative,gradient_phase)
     double precision, intent(in) :: phase_derivative
-    double precision, dimension(n_dimensions), intent(in) :: x
     double precision, dimension(n_dimensions), intent(inout) :: gradient_phase
-    double precision :: rot_x, rot_y, rot_r_squared
-
-    rot_x = x(rotation_plane_axis_1)
-    rot_y = x(rotation_plane_axis_2)
-    rot_r_squared = rot_x**2 + rot_y**2
-
-    gradient_phase(rotation_plane_axis_1) = gradient_phase(rotation_plane_axis_1) &
-      + phase_derivative * rot_y/rot_r_squared
-    gradient_phase(rotation_plane_axis_2) = gradient_phase(rotation_plane_axis_2) &
-      - phase_derivative * rot_x/rot_r_squared
+    call accum_angle_drv_into_gradient( &
+          phase_derivative, x(s,i,:), &
+          rotation_plane_axis_1, rotation_plane_axis_2, &
+          n_dimensions, &
+          gradient_phase &
+    )
   end subroutine
 
 end subroutine
 !@-node:gcross.20090827135657.1422:accumulate_gradient_fancy
+!@+node:gcross.20090915142144.1644:accum_angle_drv_into_gradient
+pure subroutine accum_angle_drv_into_gradient(&
+    derivative_of_fn_by_angle, x, &
+    rotation_plane_axis_1, rotation_plane_axis_2, &
+    n_dimensions, &
+    gradient &
+  )
+  integer, intent(in) :: rotation_plane_axis_1, rotation_plane_axis_2
+  integer, intent(in) :: n_dimensions
+  double precision, intent(in) :: derivative_of_fn_by_angle
+  double precision, dimension(n_dimensions), intent(in) :: x
+  double precision, dimension(n_dimensions), intent(inout) :: gradient
+  double precision :: rot_x, rot_y, rot_r_squared
+
+  rot_x = x(rotation_plane_axis_1)
+  rot_y = x(rotation_plane_axis_2)
+  rot_r_squared = rot_x**2 + rot_y**2
+
+  gradient(rotation_plane_axis_1) = gradient(rotation_plane_axis_1) &
+    + derivative_of_fn_by_angle * rot_y/rot_r_squared
+  gradient(rotation_plane_axis_2) = gradient(rotation_plane_axis_2) &
+    - derivative_of_fn_by_angle * rot_x/rot_r_squared
+end subroutine
+!@nonl
+!@-node:gcross.20090915142144.1644:accum_angle_drv_into_gradient
 !@+node:gcross.20090903090230.2072:accumulate_effective_potential
 subroutine accumulate_effective_potential (&
     gradient_phase, &
