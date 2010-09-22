3a9a7c154c29bb8b0b39f0a2ee4b5c55d44f3dad
Gregory Crosswhite <gcross@phys.washington.edu>
2010-09-22 16:37:35 -0700

    Performance enchancements for module xij.

--- src/utils/xij.f95
+++ src/utils/xij.f95
@@ -22,14 +22,16 @@ pure subroutine compute_xij( x, n_slices, n_particles, n_dimensions, xij2 )
   double precision, dimension (n_particles,n_particles,n_slices), intent(out) :: xij2
 
   integer :: slice, particle_1, particle_2
-  double precision :: p1x(n_dimensions)
-
+  double precision :: p1x(n_dimensions), r_sq
   do slice = 1, n_slices
     do particle_1 = 1, n_particles
       p1x = x(:,particle_1,slice)
-      do particle_2 = 1, n_particles
-        xij2(particle_2,particle_1,slice) = sum( (p1x - x(:,particle_2,slice))**2 )
+      do particle_2 = particle_1+1, n_particles
+        r_sq = sum( (p1x - x(:,particle_2,slice))**2 )
+        xij2(particle_2,particle_1,slice) = r_sq
+        xij2(particle_1,particle_2,slice) = r_sq
       end do
+      xij2(particle_1,particle_1,slice) = 0
     end do
   end do
 
@@ -43,16 +45,27 @@ pure subroutine compute_xij_pbc( x, period_length, n_slices, n_particles, n_dime
   double precision, intent(in) :: period_length
 
   integer :: slice, particle_1, particle_2
-  double precision :: p1x(n_dimensions)
-
+  double precision :: p1x(n_dimensions), r_sq
   do slice = 1, n_slices
     do particle_1 = 1, n_particles
       p1x = x(:,particle_1,slice)
-      do particle_2 = 1, n_particles
-        xij2(particle_2,particle_1,slice) = sum( wrap_around( p1x - x(:,particle_2,slice), period_length )**2 )
+      do particle_2 = particle_1+1, n_particles
+        r_sq = sum(wrap_around(p1x - x(:,particle_2,slice),period_length)**2)
+        xij2(particle_2,particle_1,slice) = r_sq
+        xij2(particle_1,particle_2,slice) = r_sq
       end do
+      xij2(particle_1,particle_1,slice) = 0
     end do
   end do
+
+contains
+
+  ! The stupid gfortran inliner will not inline this function unless it is nested...
+  elemental function wrap_around(x,period_length)
+    double precision, intent(in) :: x, period_length
+    double precision :: wrap_around
+    wrap_around = x - period_length*(floor(x/period_length - 0.5D0) + 1.0D0)
+  end function
 
 end subroutine compute_xij_pbc
 !@-node:gcross.20090805093617.1834:compute_xij_pbc
